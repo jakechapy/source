bf36988fe7dbfa517aff0caf72cbcbc841cbf382
jakechapy <52148345+jakechapy@users.noreply.github.com>
2019-06-26 11:03:13 -0600

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -305,7 +305,7 @@
             motdEnabled: false,
             motdInterval: 5,
             motd: 'Temporary Message of the Day',
-            filterChat: true,
+            filterChat: false,
             etaRestriction: false,
             welcome: true,
             opLink: null,
